c336552887293729e0f47f067b6ca97419ed0fcd
Andrey1914 <anlikajw@gmail.com>
2024-04-20 22:57:23 +0200

    added FilterAction > FilterGroup > Recipes > components

---
+++ components/Recipes/FilterGroup/FilterAction.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function FilterAction({ clearFilter }) {
+  return (
+    <div className="flex justify-between">
+      <Button
+        variant="outline"
+        className="px-6"
+        size="sm"
+        onClick={clearFilter}
+        type="button"
+      >
+        Clear
+      </Button>
+      <Button type="submit" className="px-6" size="sm">
+        Apply
+      </Button>
+    </div>
+  );
+}
