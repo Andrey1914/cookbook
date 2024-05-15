9b7ac578b390c76e859e9a02b0738b39371b0bb2
Andrey1914 <anlikajw@gmail.com>
2024-05-15 19:48:02 +0200

    added rout > slug > all > api

---
+++ app/api/all/[slug]/route.jsx
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import supabase from "@/db/client";
+
+export async function GET(req, { params: { slug } }) {
+  const { data, error } = await supabase.from(`distinct_${slug}`).select();
+
+  if (error) console.log("Failed to fetch data => API [/all/[slug]]", error);
+
+  return NextResponse.json(data);
+}
